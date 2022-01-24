a43da2c64461a1df38936f28c4cdd7ffbc11affa
MpiloNtombela <m.ntombela458@gmail.com>
2022-01-25 01:51:10 +0200

    added more details

--- src/components/pages/self/IEneabler.js
+++ src/components/pages/self/IEneabler.js
@@ -1,20 +1,35 @@
 import {useTheme} from "@emotion/react";
-import React, {useState} from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 import {useImmer} from "use-immer";
 import {useDataContext} from "../../../hooks/context";
+import Button from '../../elements/Button'
+import Text from '../../elements/Text'
+import Box from "../../layouts/Box";
 import Card from "../../layouts/Card";
 import Container from '../../layouts/Container'
-import Text from '../../elements/Text'
 import Drawer, {DrawerContainer} from '../../layouts/Drawer'
-import Button from '../../elements/Button'
 import Grid, {GridCell} from "../../layouts/Grid";
 
-const KeyPairDetails = ({k, v}) =>{
-  return(
+const caseOut = (str) => {
+  const r = /([a-z])([A-Z])/g
+  const s = str.replace(r, '$1 $2')
+  return s.toUpperCase()
+}
+
+const KeyPairDetails = ({k, v}) => {
+  return (
     <>
-      <GridCell colsSm={6}>{k.toUpperCase()}</GridCell>
-      <GridCell colsSm={6}>{v}</GridCell>
+      <GridCell colsSm={6}>
+        <Box margin={'.5rem 0'}>
+          <Text fWeight={'bold'} fSize={'.85em'} tColor={"grey"}>{caseOut(k)}</Text>
+        </Box>
+      </GridCell>
+      <GridCell colsSm={6}>
+        <Box margin={'.5rem 0'}>
+          <Text fSize={'.85em'}>{v}</Text>
+        </Box>
+      </GridCell>
     </>
   )
 }
@@ -27,6 +42,7 @@ KeyPairDetails.propTypes = {
 const IEnabler = props => {
 
   const {student} = useDataContext()
+  const {contacts} = student;
   const theme = useTheme()
 
   const [test, setTest] = useImmer({
@@ -58,34 +74,56 @@ const IEnabler = props => {
               fixed={test.fixed} onClose={handleClose}>
         <Text fSize='medium' fWeight='bold'>Mpilo</Text>
       </Drawer>
-      <Container maxWidth="xl">
+      <Container maxWidth="lg">
         <Grid gridSpacing={2}>
-          <GridCell colsMd={7}>
+          <GridCell colsMd={6} colsLg={7}>
             <Card>
-              <Text fSize="large" fWeight="bold">Main Info</Text>
-              <Grid gridSpacing={2}>
-                <GridCell colsSm={4}>
+              <Text fSize="x-large" fWeight="bold">Main Info</Text>
+              <hr/>
+              <Box marginBottom={theme.sizes.gutters[4]}>
+                <Card shadow>
+                  <Grid gridSpacing={2}>
+                    {/*<GridCell colsSm={4}>*/}
 
-                </GridCell>
-                <GridCell colsSm={8}>
-                  <Text fSize="medium" fWeight="bold">{student.firstName} {student.lastName}</Text>
-                  <Text fSize="small" fWeight="bold" tColor={theme.color.secondary}>{student.studentNumber}</Text>
-                </GridCell>
-              </Grid>
-              <Grid>
-                {Object.keys(student).map((key, idx) => {
-                  if (key.toLowerCase() !== "firstname" && key.toLowerCase() !== "lastname" && key.toLowerCase() !== "contacts") {
-                    return (
-                      <KeyPairDetails key={idx} k={key} v={student[key]}/>
-                    )
-                  }
-                })}
-              </Grid>
+                    {/*</GridCell>*/}
+                    <GridCell colsSm={8}>
+                      <Box margin={`${theme.sizes.gutters[1]} 0`}>
+                        <Text fSize="1.2em" fWeight="bold">{student.firstName} {student.lastName}</Text>
+                      </Box>
+                      <Text fSize="small" fWeight="bold" tColor={theme.color.secondary}>{student.studentNumber}</Text>
+                    </GridCell>
+                  </Grid>
+                </Card>
+              </Box>
+              <Box marginTop={theme.sizes.gutters[4]} marginBottom={theme.sizes.gutters[3]}>
+                <Grid>
+                  {Object.keys(student).map((key, idx) => {
+                    if (key.toLowerCase() !== "firstname" && key.toLowerCase() !== "lastname" && key.toLowerCase() !== "contacts") {
+                      return (
+                        <KeyPairDetails key={idx} k={key} v={student[key]}/>
+                      )
+                    }
+                  })}
+                </Grid>
+              </Box>
             </Card>
           </GridCell>
-          <GridCell colsMd={5}>
+          <GridCell colsMd={6} colsLg={5}>
             <Card>
-              <Text fSize="large" fWeight="bold">Side Info</Text>
+              <Text fSize="x-large" fWeight="bold">Contacts</Text>
+              <hr/>
+              <Box marginTop={theme.sizes.gutters[2]}>
+                <Grid>
+                  {Object.keys(contacts).map((key, idx) => {
+                    return (
+                      <KeyPairDetails key={idx} k={key} v={contacts[key]}/>
+                    )
+                  })}
+                </Grid>
+              </Box>
+              <Box marginTop={theme.sizes.gutters[3]} display={'flex'}>
+                <Button gradient rounded size="sm">update details</Button>
+              </Box>
             </Card>
           </GridCell>
         </Grid>
