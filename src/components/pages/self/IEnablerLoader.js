8701ab656aad229aae130c6a6ddd6e80ea64adb8
MpiloNtombela <m.ntombela458@gmail.com>
2022-01-27 19:20:07 +0200

    made loader more realistic

--- src/components/pages/self/IEnablerLoader.js
+++ src/components/pages/self/IEnablerLoader.js
@@ -5,26 +5,75 @@ import {useMediaQuery} from "react-responsive";
 import Text from "../../elements/Text";
 import Box from "../../layouts/Box";
 import Card from "../../layouts/Card";
+import Collapsible from "../../layouts/Collapsible";
 import Container from "../../layouts/Container";
 import Drawer, {DrawerContainer} from "../../layouts/Drawer";
 import Grid, {GridCell} from "../../layouts/Grid";
 import Skeleton from "../../layouts/Skeletons";
 
-const IEnablerLoader = ({drawerOpen = false}) => {
+const IEnablerLoader = ({drawerOpen = false, isLg}) => {
   const theme = useTheme()
 
   return (
-    <DrawerContainer drawerFixed drawerAnchor={"left"} drawerWidth={210} drawerOpen={drawerOpen}>
-      <Drawer onClose={() => {
-      }} open={drawerOpen} width={210} elevation={2}>
+    <DrawerContainer drawerFixed={isLg} drawerAnchor={"left"} drawerWidth={210} drawerOpen={drawerOpen}>
+      {drawerOpen && <Drawer onClose={() => {
+      }} open={drawerOpen} width={250} elevation={2}>
         <Box marginTop={'1rem'}>
-          {[1, 2, 3, 4, 5, 6, 7].map((num) => (
-            <Box key={num} padding={'.2rem .5rem'}>
-              <Skeleton skeletonType={'text'} style={{height: '1.75rem', background: theme.background.secondary}}/>
-            </Box>
-          ))}
+          {[1, 2, 3, 4, 5].map((num) => {
+            if (num === 1 || num === 3) {
+              return (
+                <Collapsible collapsed bgColor={theme.background.main} header={
+                  <Grid>
+                    <GridCell colsSm={2}>
+                      <Skeleton skeletonType={'avatar'}
+                                style={{
+                                  height: '1.25rem',
+                                  width: '1.25rem',
+                                  background: theme.background.secondary
+                                }}/>
+                    </GridCell>
+                    <GridCell colsSm={10}>
+                      <Skeleton skeletonType={'text'}
+                                style={{height: '1.25rem', background: theme.background.secondary}}/>
+                    </GridCell>
+                  </Grid>} key={num}>
+                  {[1, 2, 3].map((num) => (
+                    <Box key={num} padding={'.15rem .5rem .2rem  1rem'}>
+                      <Grid>
+                        <GridCell colsSm={2}>
+                          <Skeleton skeletonType={'avatar'}
+                                    style={{
+                                      height: '1.25rem',
+                                      width: '1.25rem',
+                                      background: theme.background.secondary
+                                    }}/>
+                        </GridCell>
+                        <GridCell colsSm={10}>
+                          <Skeleton skeletonType={'text'}
+                                    style={{height: '1.25rem', background: theme.background.secondary}}/>
+                        </GridCell>
+                      </Grid>
+                    </Box>
+                  ))}
+                </Collapsible>
+              )
+            } else {
+              return (<Box key={num} padding={'.15rem .5rem'}>
+                <Grid>
+                  <GridCell colsSm={2}>
+                    <Skeleton skeletonType={'avatar'}
+                              style={{height: '1.25rem', width: '1.25rem', background: theme.background.secondary}}/>
+                  </GridCell>
+                  <GridCell colsSm={10}>
+                    <Skeleton skeletonType={'text'}
+                              style={{height: '1.25rem', background: theme.background.secondary}}/>
+                  </GridCell>
+                </Grid>
+              </Box>)
+            }
+          })}
         </Box>
-      </Drawer>
+      </Drawer>}
       <Container maxWidth="lg">
         <Grid gridSpacing={2}>
           <GridCell colsMd={6} colsLg={7}>
@@ -45,7 +94,10 @@ const IEnablerLoader = ({drawerOpen = false}) => {
                 <Grid gridSpacing={2}>
                   {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
                     <GridCell key={num} colsSm={6}>
-                      <Skeleton skeletonType="text"/>
+                      <Skeleton style={{width: num % 2 !== 0 ?
+                          num === 3 || num === 7 ?
+                            "30%" : '60%'
+                          : '85%'}} skeletonType="text"/>
                     </GridCell>
                   ))}
                 </Grid>
@@ -59,7 +111,7 @@ const IEnablerLoader = ({drawerOpen = false}) => {
                 <Grid gridSpacing={2}>
                   {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
                     <GridCell key={num} colsSm={6}>
-                      <Skeleton skeletonType="text"/>
+                      <Skeleton style={{width: num % 2 !== 0 ? '60%' : '100%'}} skeletonType="text"/>
                     </GridCell>
                   ))}
                 </Grid>
@@ -84,7 +136,8 @@ const IEnablerLoader = ({drawerOpen = false}) => {
 };
 
 IEnablerLoader.propTypes = {
-  drawerOpen: PropTypes.bool
+  drawerOpen: PropTypes.bool.isRequired,
+  isLg: PropTypes.bool.isRequired,
 }
 
 export default IEnablerLoader;
