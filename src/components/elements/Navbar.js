3568526e8648e72c7de50d2ed5af2ec95f483616
MpiloNtombela <m.ntombela458@gmail.com>
2022-01-31 20:48:04 +0200

    added maxBreak, a max breakpoint for navbar

--- src/components/elements/Navbar.js
+++ src/components/elements/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 /**
- * @description height of the top navbar (assumes using 'rem' size)
+ * @description height of the navbar (assumes using 'rem' size)
  */
 export const NAV_HEIGHT = 3.25;
 
@@ -17,6 +17,7 @@ export const StyledNavLink = styled(NavLink)`
   text-decoration: none;
   margin: 0 ${({theme}) => theme.sizes.gutters[2]};
   font-weight: 500;
+
   &.active {
     color: ${props => props.theme.palette.primary.main} !important;
   }
@@ -26,7 +27,7 @@ export const NavbarLinks = styled.div`
   flex: 1;
   display: flex;
   justify-content: flex-end;
-  
+
   .active-parent {
     color: ${props => props.theme.palette.secondary.main}
   }
@@ -36,7 +37,7 @@ const StyledNavbar = styled.nav`
   display: flex;
   position: ${({navStyle}) => navStyle ? navStyle : 'static'};
   background: ${({bgColor, theme}) => bgColor ? bgColor : theme.background.main};
-  color: ${({textColor, theme}) => textColor ? textColor :theme.color.secondary};
+  color: ${({textColor, theme}) => textColor ? textColor : theme.color.secondary};
   box-shadow: 0px -5px 20px 0px rgba(0, 0, 0,.${props => props.navElevation});
   min-height: ${NAV_HEIGHT}rem;
   max-height: ${NAV_HEIGHT}rem;
@@ -47,12 +48,12 @@ const StyledNavbar = styled.nav`
   align-items: center;
   transition: all .5s ease-in-out;
   overflow: hidden;
-  
+
   ${StyledNavLink} {
     color: inherit;
   }
 
-  @media (max-width: ${props => props.theme.breakpoints.sm}px) {
+  @media (max-width: ${({maxBreak, theme}) => maxBreak ? theme.breakpoints[maxBreak] : 0}px) {
     flex-direction: column;
     align-items: normal;
     max-height: ${({open}) => open ? `70vh` : `${NAV_HEIGHT}rem`};
@@ -60,6 +61,7 @@ const StyledNavbar = styled.nav`
       flex-direction: column;
       display: ${({open}) => open ? `flex` : 'none'};
     }
+
     ${StyledNavLink} {
       margin: 0;
       padding: ${({theme}) => theme.sizes.gutters[2]};
@@ -82,7 +84,7 @@ const StyledNavbar = styled.nav`
           transform: translateX(-135%);
         }
       }
-      
+
       &.active {
         background: ${props => props.theme.palette.primary.main};
         color: ${props => props.theme.palette.primary.contrastText} !important;
@@ -101,13 +103,13 @@ const StyledNavbar = styled.nav`
 
 `
 
-const Navbar = ({logo, elevation = 0, children}) => {
+const Navbar = ({logo, maxBreak = "sm", elevation = 0, children}) => {
   return (
-    <StyledNavbar navElevation={elevation} open={true}>
+    <StyledNavbar navElevation={elevation} open={true} maxBreak={maxBreak}>
       <Brand>
         {logo}
       </Brand>
-        {children}
+      {children}
     </StyledNavbar>
   );
 };
@@ -115,7 +117,8 @@ const Navbar = ({logo, elevation = 0, children}) => {
 Navbar.propTypes = {
   children: PropTypes.node,
   elevation: PropTypes.oneOf([0, 1, 2, 3, 4]),
-  logo: PropTypes.node
+  logo: PropTypes.node,
+  maxBreak: PropTypes.oneOf(["sm", "md", "lg", "xl", "xxl", false])
 }
 
 export default Navbar;
