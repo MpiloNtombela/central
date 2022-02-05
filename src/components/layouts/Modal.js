253e9af2d86097257dd16baee9b00f87d7d29e6c
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-06 01:05:08 +0200

    Added false option for maxWidth which removes any margin and padding and set modal content full screen. Also setting radius prop false set radius to 0.

--- src/components/layouts/Modal.js
+++ src/components/layouts/Modal.js
@@ -6,11 +6,14 @@ import Container from "./Container";
 const StyledModal = styled.div`
   background: ${({theme}) => theme.background.secondary};
   width: 100%;
-  border-radius: ${({theme, radius}) => theme.sizes.radius[radius]};
+  border-radius: ${({theme, radius}) => radius ? theme.sizes.radius[radius] : 0};
   padding: ${({theme}) => theme.sizes.gutters[2]};
   box-sizing: border-box;
   box-shadow: 0px -5px 20px 0px rgba(0, 0, 0,.${({elevation}) => elevation});
-  max-height: ${({scrollOverlay}) => scrollOverlay ? `fit-content` : `calc(100vh - 2rem)`};
+  max-height: ${({
+                   scrollOverlay,
+                   maxWidth
+                 }) => scrollOverlay ? `fit-content` : maxWidth ? `calc(100vh - 2rem)` : '100vh'};
   overflow-y: ${({scrollOverlay}) => scrollOverlay ? `hidden` : 'auto'};
 `
 
@@ -25,7 +28,7 @@ const StyledModalOverlay = styled.div`
   bottom: ${({open}) => open ? 0 : `-100%`};
   right: ${({open}) => open ? 0 : `-100%`};
   transition: opacity .5s ease-in-out;
-  padding: ${({theme}) => theme.sizes.gutters[4]} 0;
+  padding: ${({theme, maxWidth}) => maxWidth ? `${theme.sizes.gutters[4]}` : 0} 0;
   overflow-y: ${({scrollOverlay}) => scrollOverlay ? `auto` : 'hidden'};
   z-index: ${({theme, open}) => open ? theme.sizes.zIndex.modal : -1};
 `
@@ -33,10 +36,10 @@ const StyledModalOverlay = styled.div`
 
 const Modal = ({open, centerVert, scrollOverlay = true, elevation = 1, radius = 'sm', maxWidth = 'sm', children}) => {
   return (
-    <StyledModalOverlay open={open} scrollOverlay={scrollOverlay}>
+    <StyledModalOverlay open={open} scrollOverlay={scrollOverlay} maxWidth={maxWidth}>
       <Container maxWidth={maxWidth}
                  style={{display: centerVert ? 'flex' : 'block', alignItems: 'center'}}>
-        <StyledModal radius={radius} elevation={elevation} scrollOverlay={scrollOverlay}>
+        <StyledModal radius={radius} elevation={elevation} scrollOverlay={scrollOverlay} maxWidth={maxWidth}>
           {children}
         </StyledModal>
       </Container>
@@ -46,8 +49,8 @@ const Modal = ({open, centerVert, scrollOverlay = true, elevation = 1, radius = 
 
 Modal.propTypes = {
   open: PropTypes.bool.isRequired,
-  radius: PropTypes.oneOf(['sm', 'md', 'lg', 'xl', 'xxl']),
-  maxWidth: PropTypes.oneOf(['sm', 'md', 'lg', 'xl']),
+  radius: PropTypes.oneOf(["sm", "md", "lg", "xl", "xxl", false]),
+  maxWidth: PropTypes.oneOf(["sm", "md", "lg", "xl", false]),
   children: PropTypes.node,
   centerVert: PropTypes.bool,
   elevation: PropTypes.oneOf([1, 2, 3, 4]),
