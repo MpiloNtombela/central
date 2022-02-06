6d9eaccfea0a09d83bc138ccb30401636e3f1ecf
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-06 02:21:40 +0200

    Added Modal Header and Modal Content components

--- src/components/layouts/Modal.js
+++ src/components/layouts/Modal.js
@@ -1,13 +1,40 @@
+import {useTheme} from "@emotion/react";
 import styled from "@emotion/styled";
 import PropTypes from "prop-types";
 import React from 'react';
+import {NAV_HEIGHT} from "../elements/Navbar";
+import Text from "../elements/Text";
+import Box from "./Box";
 import Container from "./Container";
+
+const StyledModalHeader = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  background: ${({theme}) => theme.background.secondary};
+  margin-bottom: ${({theme}) => theme.sizes.gutters[2]};
+  font-size: ${({text}) => text ? '1.2rem' : 'inherit'};
+  font-weight: ${({text}) => text ? '600' : 'inherit'};
+  padding: 0 ${({theme}) => theme.sizes.gutters[3]};
+  box-shadow: 0px -5px 20px 0px rgba(0, 0, 0,.${({elevation}) => elevation});
+  box-sizing: border-box;
+  height: ${NAV_HEIGHT}rem;
+  position: ${({sticky}) => sticky ? 'sticky' : 'static'};
+  top: ${({sticky}) => sticky ? 0 : 'auto'};
+  left: ${({sticky}) => sticky ? 0 : 'auto'};
+  right: ${({sticky}) => sticky ? 0 : 'auto'};
+`
+
+const StyledModalContent = styled.div`
+  width: 100%;
+  padding: ${({theme}) => theme.sizes.gutters[3]};
+  box-sizing: border-box;
+`
 
 const StyledModal = styled.div`
   background: ${({theme}) => theme.background.secondary};
   width: 100%;
   border-radius: ${({theme, radius}) => radius ? theme.sizes.radius[radius] : 0};
-  padding: ${({theme}) => theme.sizes.gutters[2]};
   box-sizing: border-box;
   box-shadow: 0px -5px 20px 0px rgba(0, 0, 0,.${({elevation}) => elevation});
   max-height: ${({
@@ -33,6 +60,33 @@ const StyledModalOverlay = styled.div`
   z-index: ${({theme, open}) => open ? theme.sizes.zIndex.modal : -1};
 `
 
+export const ModalHeader = ({text, hasBack, sticky, hasClose = true, elevation = 1, children}) => {
+  const theme = useTheme()
+  return (
+    <StyledModalHeader text={text} elevation={elevation} sticky={sticky}>
+      {hasBack && <Box marginRight={theme.sizes.gutters[3]}>
+        &larr;
+      </Box>}
+      <Box style={{
+        flex: 2,
+        textOverflow: 'ellipsis',
+        overflow: text ? 'hidden' : 'inherit',
+        whiteSpace: text ? 'nowrap' : 'inherit'
+      }}>{children ? children : text}</Box>
+      {hasClose && <Box marginLeft={theme.sizes.gutters[3]}>
+        <Text fSize={'small'} fWeight={'bold'}>CLOSE</Text>
+      </Box>}
+    </StyledModalHeader>
+  )
+}
+
+export const ModalContent = ({children}) => {
+  return (
+    <StyledModalContent>
+      {children}
+    </StyledModalContent>
+  )
+}
 
 const Modal = ({open, centerVert, scrollOverlay = true, elevation = 1, radius = 'sm', maxWidth = 'sm', children}) => {
   return (
@@ -47,13 +101,26 @@ const Modal = ({open, centerVert, scrollOverlay = true, elevation = 1, radius = 
   );
 };
 
+ModalHeader.propTypes = {
+  text: PropTypes.string,
+  hasBack: PropTypes.bool,
+  hasClose: PropTypes.bool,
+  sticky: PropTypes.bool,
+  elevation: PropTypes.oneOf([0, 1, 2, 3, 4]),
+  children: PropTypes.node,
+}
+
+ModalContent.propTypes = {
+  children: PropTypes.node,
+}
+
 Modal.propTypes = {
   open: PropTypes.bool.isRequired,
   radius: PropTypes.oneOf(["sm", "md", "lg", "xl", "xxl", false]),
   maxWidth: PropTypes.oneOf(["sm", "md", "lg", "xl", false]),
   children: PropTypes.node,
   centerVert: PropTypes.bool,
-  elevation: PropTypes.oneOf([1, 2, 3, 4]),
+  elevation: PropTypes.oneOf([0, 1, 2, 3, 4]),
   scrollOverlay: PropTypes.bool,
 }
 
