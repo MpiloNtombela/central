b847fe461296e1b67a588a9c3689ae288433e21a
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-06 19:05:48 +0200

    Updated announcement component to use collapsible

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,41 +1,104 @@
 import {useTheme} from "@emotion/react";
+import PropTypes from "prop-types";
 import React, {useState} from 'react';
 import {useDataContext} from "../../hooks/context";
 import Button from "../elements/Button";
 import Text from "../elements/Text";
 import Box from "../layouts/Box";
 import Card from "../layouts/Card";
+import Collapsible from "../layouts/Collapsible";
 import Container from "../layouts/Container";
 import Grid, {GridCell} from "../layouts/Grid";
 import Modal, {ModalContent, ModalHeader} from "../layouts/Modal";
+import TabContext, {Tab, TabContent, Tabs} from "../layouts/Tabs";
+
+const Announcement = ({announcement, theme, collapsed}) => {
+  return (
+    <Box margin={`${theme.sizes.gutters[2]} 0`}>
+      <Card>
+        <Collapsible
+          collapsed={collapsed}
+          isHighlighted={false}
+          header={<Text style={{textTransform: 'capitalize'}} fWeight={'500'}>
+            {announcement.title}</Text>}>
+          <Text fSize={'1rem'} tColor={theme.color.secondary}>{announcement?.content}</Text>
+          {announcement?.actions.length > 0 &&
+          <Box display={'flex'} justifyContent={'flex-end'} marginTop={theme.sizes.gutters[4]}>
+            {announcement?.actions.map((action, idx) => {
+              return (
+                <Box key={idx} marginLeft={theme.sizes.gutters[2]}>
+                  {action.type === 'link' ?
+                    <a style={{
+                      fontWeight: '700',
+                      fontSize: 'small',
+                      textTransform: 'uppercase',
+                      color: theme.palette.primary.main
+                    }}
+                       href={action.destination}>{action.name}</a> :
+                    <Button size={'sm'} rounded>{action.name}</Button>}
+                </Box>)
+            })}
+          </Box>}
+        </Collapsible>
+      </Card>
+    </Box>
+  )
+}
+
+Announcement.propTypes = {
+  announcement: PropTypes.object,
+  theme: PropTypes.any,
+  collapsed: PropTypes.bool,
+}
 
 const Announcements = () => {
   const data = useDataContext()
   const announcements = [...data.announcements]
-  const [ann, setAnn] = useState(null)
+  const [showMore, setShowMore] = useState(false)
   const theme = useTheme()
-  const handleOpen = (id) => {
-    const a = announcements.find(a => a.id === id)
-    setAnn(a)
+
+  const showMoreToggle = () => {
+    setShowMore(!showMore)
   }
   return (
     <>
-      {announcements.sort((aa, ab) => aa.importantScore < ab.importantScore).map((a, idx) => {
+      {announcements.sort((aa, ab) => {
+        if (aa.pinned || aa.importantScore > ab.importantScore) {
+          return -1
+        } else {
+          return 1
+        }
+      }).slice(0, 5).map((announcement, idx) => {
         return (
-          <div onClick={() => handleOpen(a.id)} key={idx}>
-            <Card>
-              <Text fWeight={'500'}>{a.title}</Text>
-              <Box marginTop={theme.sizes.gutters[2]}>
-                <Text tAlign={'end'} fSize={'xx-small'} tColor={'#888'}>{a.date}</Text>
-              </Box>
-            </Card>
-          </div>
+          <Announcement key={announcement.id} announcement={announcement} collapsed={idx === 0} theme={theme}/>
         )
       })}
-      <Modal open={ann !== null} rounded={'md'} maxWidth={"sm"} elevation={2} onClose={() => setAnn(null)}>
-        <ModalHeader text={ann?.title} hasClose onCloseClick={() => setAnn(null)}/>
+      <Box marginTop={theme.sizes.gutters[4]}>
+        <Button onClick={showMoreToggle} color={"primary"} size={'sm'} rounded block>show more</Button>
+      </Box>
+      <Modal open={showMore} onClose={showMoreToggle} maxWidth={'md'} rounded={"md"}>
+        <ModalHeader text={"Announcements"} elevation={4} onCloseClick={showMoreToggle}/>
         <ModalContent>
-          <Text fSize={theme.typography.fontsize.paragraph}>{ann?.content}</Text>
+          <TabContext>
+            <Tabs isFixed>
+              <Tab value={'latest'}>latest</Tab>
+              <Tab value={'previous'}>previous</Tab>
+            </Tabs>
+            <TabContent value={'latest'}>
+              {announcements.filter(a => a.active).map((announcement, idx) => {
+                return (
+                  <Announcement key={announcement.id} announcement={announcement} collapsed={idx === 0} theme={theme}/>
+                )
+              })}
+            </TabContent>
+            <TabContent value={'previous'}>
+              {announcements.filter(a => !a.active).map((announcement, idx) => {
+                return (
+                  <Announcement key={announcement.id} announcement={announcement} collapsed={idx === 0} theme={theme}/>
+                )
+              })}
+            </TabContent>
+          </TabContext>
         </ModalContent>
       </Modal>
     </>
@@ -43,23 +106,17 @@ const Announcements = () => {
 }
 
 const Home = () => {
-    const theme = useTheme()
     return (
-      <Container maxWidth='xl'>
+      <Container maxWidth='xl' navPadding>
         <Grid gridSpacing={2}>
           <GridCell colsLg={7} colsXl={8}>
             <Card>
-              <Text fSize={theme.typography.fontsize.lg} fWeight={'bold'}>Latest Ads</Text>
+              <Text fSize={'1.25rem'} fWeight={'bold'}>Latest Ads</Text>
             </Card>
           </GridCell>
           <GridCell colsLg={5} colsXl={4}>
-            <Card shadow={false}>
-              <Text fSize={theme.typography.fontsize.lg} fWeight={'bold'}>Announcements</Text>
-              <Announcements/>
-              <Box marginTop={theme.sizes.gutters[4]}>
-                <Button color={"primary"} size={'sm'} rounded block>Load more</Button>
-              </Box>
-            </Card>
+            <Text fSize={'1.25rem'} fWeight={'bold'}>Announcements</Text>
+            <Announcements/>
           </GridCell>
         </Grid>
       </Container>
