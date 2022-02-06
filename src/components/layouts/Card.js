e748f1ecd85651649485903b8f9971b4c26f3406
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-06 15:07:38 +0200

    added palette colors as background option

--- src/components/layouts/Card.js
+++ src/components/layouts/Card.js
@@ -1,14 +1,19 @@
 import styled from "@emotion/styled";
 import React from "react";
 import PropTypes from "prop-types";
-import { css } from "@emotion/react";
+import {css} from "@emotion/react";
 
-const cardBase = ({ theme, maxWidth, shadow }) => css`
-  background: ${theme.background.secondary};
+const cardBase = ({theme, maxWidth, bgColor, shadow}) => css`
+  background: ${bgColor ?
+          bgColor === "main" ? theme.bgColor.main
+                  : theme.palette[bgColor].main
+          : theme.bgColor.secondary};
   border-radius: ${theme.sizes.radius.md};
   box-shadow: ${shadow ? "0px 0px 10px 0px hsla(0, 0%, 0%, .2)" : "none"};
   font-family: ${theme.typography.fontFamily};
-  color: ${theme.color.main};
+  color: ${bgColor ? bgColor === "main" ? theme.color.main 
+          : theme.palette[bgColor].contrastText
+          : theme.color.main};
   max-width: ${theme.breakpoints[maxWidth]}px;
   z-index: 10;
 `;
@@ -24,15 +29,16 @@ const StyledCard = styled.div`
 
 `;
 
-const Card = ({ maxWidth = "xl", shadow = true, children }) => {
+const Card = ({bgColor, maxWidth = "xl", shadow = true, children}) => {
   return (
-    <StyledCard maxWidth={ maxWidth } shadow={ shadow }>
-      { children }
+    <StyledCard maxWidth={maxWidth} shadow={shadow} bgColor={bgColor}>
+      {children}
     </StyledCard>
   );
 };
 
 Card.propTypes = {
+  bgColor: PropTypes.oneOf(["main", "primary", "secondary", "success", "warning", "danger", "info"]),
   maxWidth: PropTypes.oneOf(["sm", "md", "lg", "xl", "xxl"]),
   shadow: PropTypes.bool,
   children: PropTypes.node,
