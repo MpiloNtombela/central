4ed728a520e25bb9879815f922f47549d0774bc2
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-12 13:15:21 +0200

    Added Drawer for viewing of Ad

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,8 @@ import {useTheme} from "@emotion/react";
 import PropTypes from "prop-types";
 import React, {useState} from 'react';
 import {MdAdminPanelSettings, MdCalendarToday, MdClose, MdPushPin} from "react-icons/md";
+import {useMediaQuery} from "react-responsive";
+import {useImmer} from "use-immer";
 import {useDataContext} from "../../hooks/context";
 import Button from "../elements/Button";
 import Table, {TableData, TableHead, TableRow, TBody, THead} from "../elements/Table";
@@ -11,6 +13,7 @@ import Card from "../layouts/Card";
 import Chip from "../layouts/Chip";
 import Collapsible from "../layouts/Collapsible";
 import Container from "../layouts/Container";
+import Drawer from "../layouts/Drawer";
 import Grid, {GridCell} from "../layouts/Grid";
 import Modal, {ModalContent, ModalHeader} from "../layouts/Modal";
 import Skeleton from "../layouts/Skeletons";
@@ -212,53 +215,133 @@ const AllAds = ({ads, openAds, handleClose}) => {
   )
 }
 
-const Home = () => {
-    const [openAds, setOpenAds] = useState(false)
-    const {ads} = useDataContext()
-    const theme = useTheme()
-
-    const handleOpenAdsToggle = () => {
-      setOpenAds(!openAds)
-    }
-
-    return (
-      <Container maxWidth='xl' navPadding>
-        <Grid gridSpacing={2}>
-          <GridCell colsLg={7} colsXl={8}>
-            <Card>
-              <Table striped isHover capHead captionText={`10 of ${ads.length} ads`} captionSide={'bottom'}
-                     headColor={'secondary'}
-                     tableSize={'lg'}>
-                <THead>
-                  <TableRow>
-                    <TableHead>Latest Ads</TableHead>
-                  </TableRow>
-                </THead>
-                <TBody>
-                  {ads.slice(0, 10).map((ad) => {
-                    return (
-                      <TableRow key={ad.id}>
-                        <TableData>{ad.title}</TableData>
-                      </TableRow>)
-                  })}
-                </TBody>
-              </Table>
-              <Box display={'flex'} justifyContent={'flex-end'} marginTop={theme.sizes.gutters[4]}>
-                <Button size={'sm'} rounded onClick={handleOpenAdsToggle}>show all ads</Button>
-              </Box>
-            </Card>
-          </GridCell>
-          <GridCell colsLg={5} colsXl={4}>
-            <Text fSize={'1.25rem'} fWeight={'bold'}>Announcements</Text>
-            <Announcements/>
-          </GridCell>
+const Ad = ({ad, theme}) => {
+  return (
+    <Box padding={theme.sizes.gutters[3]}>
+      <Text fSize={'medium'} fWeight={'bold'} tColor={theme.palette.info.main}>{ad.title}</Text>
+      <Box marginTop={theme.sizes.gutters[2]} marginBottom={theme.sizes.gutters[4]}>
+        <Grid>
+          {Object.keys(ad).map((key, idx) => {
+            if (key.toLowerCase() !== 'id' && key.toLowerCase() !== 'title') {
+              return (
+                <React.Fragment key={idx}>
+                  <GridCell colsSm={6} colsMd={5}>
+                    <Box margin={'.5rem 0'}>
+                      <Text fWeight={'bold'} fSize={'.85em'}
+                            tColor={"grey"}>{caseOut(key).toUpperCase()}</Text>
+                    </Box>
+                  </GridCell>
+                  <GridCell colsSm={6} colsMd={7}>
+                    <Box margin={'.5rem 0'}>
+                      <Text fSize={'.85em'}>{ad[key]}</Text>
+                    </Box>
+                  </GridCell>
+                </React.Fragment>
+              )
+            }
+          })}
         </Grid>
-        {openAds && <AllAds handleClose={handleOpenAdsToggle} openAds={openAds} ads={ads}/>}
+      </Box>
+      <Button color={'danger'} rounded block size={'sm'}>report ad</Button>
+    </Box>
+  )
+}
 
-      </Container>
-    );
+const Home = () => {
+  const {ads} = useDataContext()
+  const theme = useTheme()
+  const isSm = useMediaQuery({maxWidth: theme.breakpoints.sm})
+  const [openAds, setOpenAds] = useState(false)
+  const [openedAd, setOpenedAd] = useImmer({
+    ad: null,
+    isLoading: true,
+    open: false,
+  })
+
+
+  const handleOpenAd = (id) => {
+    if (openedAd?.ad?.id === id) {
+      setOpenedAd(draft => {
+        draft.open = true
+        draft.isLoading = false
+      })
+    } else {
+      setOpenedAd(draft => {
+        draft.open = true
+        draft.isLoading = true
+      })
+      const ad = ads.find(a => a.id === id)
+      setTimeout(() => {
+        if (ad) {
+          setOpenedAd(draft => {
+            draft.ad = ad
+            draft.isLoading = false
+          })
+        } else {
+          setOpenedAd(draft => {
+            draft.ad = null
+            draft.isLoading = false
+          })
+        }
+      }, 2000)
+    }
   }
-;
+
+  const handleCloseAd = () => {
+    setOpenedAd(draft => {
+      draft.open = false
+    })
+  }
+
+  const handleOpenAdsToggle = () => {
+    setOpenAds(!openAds)
+  }
+
+  return (
+    <Container maxWidth='xl' navPadding>
+      <Grid gridSpacing={2}>
+        <GridCell colsLg={7} colsXl={8}>
+          <Card>
+            <Table striped isHover capHead captionText={`10 of ${ads.length} ads`} captionSide={'bottom'}
+                   headColor={'secondary'}
+                   tableSize={'lg'}>
+              <THead>
+                <TableRow>
+                  <TableHead>Latest Ads</TableHead>
+                </TableRow>
+              </THead>
+              <TBody>
+                {ads.slice(0, 10).map((ad) => {
+                  return (
+                    <TableRow key={ad.id} onClick={() => handleOpenAd(ad.id)}>
+                      <TableData>{ad.title}</TableData>
+                    </TableRow>)
+                })}
+              </TBody>
+            </Table>
+            <Box display={'flex'} justifyContent={'flex-end'} marginTop={theme.sizes.gutters[4]}>
+              <Button size={'sm'} rounded onClick={handleOpenAdsToggle}>show all ads</Button>
+            </Box>
+          </Card>
+        </GridCell>
+        <GridCell colsLg={5} colsXl={4}>
+          <Text fSize={'1.25rem'} fWeight={'bold'}>Announcements</Text>
+          <Announcements/>
+        </GridCell>
+      </Grid>
+      {openAds && <AllAds handleClose={handleOpenAdsToggle} openAds={openAds} ads={ads}/>}
+      <Drawer rounded={isSm} height={isSm ? '60%' : '100%'} width={isSm ? '100%' : 300} onClose={handleCloseAd}
+              open={openedAd.open}
+              anchor={isSm ? 'bottom' : 'right'}>
+        {openedAd.isLoading ?
+          <Text fSize={'medium'} fWeight={'bold'}>Loading...</Text>
+          : openedAd.ad ? <Ad ad={openedAd.ad} theme={theme}/>
+            : <Text fSize={'medium'} fWeight={'bold'}>Ad not found</Text>
+        }
+      </Drawer>
+    </Container>
+  );
+}
 
 
 Announcement.propTypes = {
@@ -271,6 +354,19 @@ AllAds.propTypes = {
   ads: PropTypes.array.isRequired,
   openAds: PropTypes.bool.isRequired,
   handleClose: PropTypes.func.isRequired,
+}
+
+Ad.propTypes = {
+  ad: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    postedBy: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  }),
+  theme: PropTypes.any
 }
 
 export default Home;
