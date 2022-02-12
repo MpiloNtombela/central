0143be9e21cd72da2d190de5af86c685207079ff
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-12 15:50:43 +0200

    Moved button styles to ButtonBase to allow reuse

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -1,68 +1,69 @@
+import {css} from "@emotion/react";
 import styled from "@emotion/styled";
 import PropTypes from "prop-types";
 import React from "react";
 
-const StyledButton = styled.button(
-  ({theme, color, gradient, size, outlined, rounded, block, disabled}) => {
-    const b = theme.palette;
-    const c = b[color];
+export const ButtonBase = ({theme, color, gradient, size, outlined, rounded, block, disabled}) => {
+  const b = theme.palette;
+  const c = b[color];
 
-    return `
+  return css`
+    color: ${!outlined ? c.contrastText : c.main};
+    background: ${!outlined ?
+            gradient ? `linear-gradient(140deg, ${b.primary.main}, ${b.secondary.main})`
+                    : c.main
+            : "transparent"
+    };
+    padding: ${size === "sm" ? ".25rem 1rem" : ".45rem 1.25rem"};
+    text-transform: uppercase;
+    font-weight: 500;
+    line-height: 1.5;
+    border: 2px solid ${disabled ? '#888' : c.main};
+    border-radius: ${rounded ? "9999rem" : theme.sizes.radius};
+    font-size: 0.875rem;
+    box-sizing: border-box;
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+    text-align: center;
+    white-space: nowrap;
+    letter-spacing: 0.02857em;
+    box-shadow: 0 0 5px 0 hsla(0, 0%, 0%, .5);
+    min-width: ${block ? '100%' : '64px'};
 
-  color: ${!outlined ? c.contrastText : c.main};
-  background: ${!outlined
-      ? gradient
-        ? `linear-gradient(140deg, ${b.primary.main}, ${b.secondary.main})`
-        : c.main
-      : "transparent"
-    };
-  padding: ${size === "sm" ? ".25rem 1rem" : ".45rem 1.25rem"};
-  text-transform: uppercase;
-  font-weight: 500;
-  line-height: 1.25;
-  border: 2px solid ${disabled ? '#888' : c.main};
-  border-radius: ${rounded ? "9999rem" : theme.sizes.radius};
-  min-width: max-content;
-  font-size: 0.875rem;
-  box-sizing: border-box;
-  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-  line-height: 1.75;
-  text-align: center;
-  white-space: nowrap;
-  letter-spacing: 0.02857em;
-  box-shadow: 0px 0px 5px 0px hsla(0, 0%, 0%, .5);
-  min-width: ${block ? '100%' : '64px'};
-  transition: background, .5s, ease-in-out;
+    &:hover {
+      background: ${(outlined ? c.glass : gradient
+              ? `linear-gradient(315deg, ${b.primary.main}, ${b.secondary.main})`
+              : c.light)
+      };
+      cursor: pointer;
+      outline: none;
+      border-color: ${disabled ? '#888' : outlined ? c.main : c.light};
+    }
+  ;
 
-  &:hover {
-    background: ${(outlined ? c.glass : gradient
-      ? `linear-gradient(315deg, ${b.primary.main}, ${b.secondary.main})`
-      : c.light)
-    };
-    cursor: pointer;
-    outline: none;
-    border-color: ${disabled ? '#888' : outlined ? c.main : c.light};
-  };
-  &:disabled {
-    background: ${outlined ? "transparent" : '#888'};
-    color: ${outlined ? '#888' : '#ccc'};
-    cursor: default;
-    border-color: #888
-    box-shadow: 0px 0px 2px 0px hsla(0, 0%, 0%, .35);
-    opacity: 1;
-  }
-  &:focus{
-    background: ${(outlined ? c.glass : gradient
-      ? `linear-gradient(315deg, ${b.primary.main}, ${b.secondary.main})`
-      : c.dark)
-    };
-    box-shadow: 0px 0px .75rem ${c.dark};
-    border-color: ${c.dark}
-  }
+    &:disabled {
+      background: ${outlined ? "transparent" : '#888'};
+      color: ${outlined ? '#888' : '#ccc'};
+      cursor: default;
+      border-color: #888;
+      box-shadow: 0 0 2px 0 hsla(0, 0%, 0%, .35);
+      opacity: 1;
+    }
+
+    &:focus {
+      background: ${(outlined ? c.glass : gradient
+              ? `linear-gradient(315deg, ${b.primary.main}, ${b.secondary.main})`
+              : c.dark)
+      };
+      box-shadow: 0 0 .75rem ${c.dark};
+      border-color: ${c.dark}
+    }
+  `
+}
+
+const StyledButton = styled.button`
+  ${ButtonBase}
 `;
-  }
-);
 
 const Button = ({
                   color = "primary",
