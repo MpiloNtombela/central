4d38c8763fd8a56f9b7d463a8c342b4ba3d4b5b7
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-15 00:51:11 +0200

    Added Class and Final Mark components

---
+++ src/components/pages/achievement/ClassMarks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClassMarks = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default ClassMarks;
---
+++ src/components/pages/achievement/FinalMarks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FinalMarks = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default FinalMarks;
