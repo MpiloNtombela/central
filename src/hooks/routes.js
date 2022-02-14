b5a1c33e938985cbf88fb1668a97e121dcd2028e
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-14 23:52:05 +0200

    Added small routes hook

---
+++ src/hooks/routes.js
@@ -0,0 +1,9 @@
+/**
+ * @description gets the subRoute path for the given route name
+ * @param route parent route
+ * @param name name of the sub route
+ * @returns {string} path
+ */
+export const useGetSubPath = (route, name) => {
+  return route.subRoutes.find(r => r.name.toLowerCase() === name)?.path
+}
