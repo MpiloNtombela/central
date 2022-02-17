dbb05a0e30b9aae8fa9802017cfce56d06bc2d44
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-17 23:47:45 +0200

    Added context

--- src/components/pages/achievement/FinalMarks.js
+++ src/components/pages/achievement/FinalMarks.js
@@ -1,5 +1,6 @@
 import {useTheme} from "@emotion/react";
 import React, {useEffect, useState} from 'react';
+import {useImmerReducer} from "use-immer";
 import Table, {TableData, TableHead, TableRow, TBody, THead} from "../../elements/Table";
 import Text from "../../elements/Text";
 import Box from "../../layouts/Box";
@@ -18,13 +19,44 @@ const randMarks = (min, max, len = 4) => {
   return marx
 }
 
+
+const FinalContext = React.createContext()
+const FinalDispatch = React.createContext()
+
+const ADD_DEAN = 'ADD_DEAN'
+const ADD_PASS = 'ADD_PASS'
+const ADD_FAIL = 'ADD_FAIL'
+
+const initData = {
+  risks: false,
+  deans: 0,
+  fails: 0,
+  passed: 0
+}
+
+const finalReducer = (draft, action) => {
+  switch (action.type) {
+    case ADD_DEAN:
+      draft.deans = action.payload
+      break
+    case ADD_PASS:
+      draft.passed = action.payload
+      break
+    case ADD_FAIL:
+      draft.pass = action.payload
+      break
+  }
+}
+
+
 const Semester = ({yr, sem, theme}) => {
-  const marks = randMarks(30, 100)
+  const [marks] = useState(randMarks(30, 100))
   const [loading, setLoading] = useState(true)
   useEffect(() => {
     setTimeout(() => {
       setLoading(false)
     }, 2500)
+
   }, [])
 
   if (loading) {
@@ -78,41 +110,46 @@ const Semester = ({yr, sem, theme}) => {
 
 const FinalMark = () => {
   const theme = useTheme()
+  const [finalData, dispatch] = useImmerReducer(finalReducer, initData)
   return (
-    <Container maxWidth={'md'}>
-      <Box marginTop={theme.sizes.gutters[4]}/>
-      {[2021, 2020, 2019, 2018].map((yr, idx) => (
-        <Card key={yr} style={{marginBottom: theme.sizes.gutters[4], paddingLeft: 0, paddingRight: 0}}>
-          <Collapsible collapsed={idx === 0}
-                       isHighlighted={false}
-                       header={
-                         <Box paddingTop={theme.sizes.gutters[2]} paddingBottom={theme.sizes.gutters[2]}>
-                           <Text fSize={'medium'}
-                                 fWeight={'bold'}
-                                 tColor={theme.palette.dark.main}>
-                             {yr === 2018 ? "Bachelor of Arts [B-ART]"
-                               : "Bachelor of Social Sciences [BSS]"} - {yr}</Text>
-                         </Box>}
-                       marginBottom={theme.sizes.gutters[4]}>
-            <TabContext>
-              <Box paddingLeft={theme.sizes.gutters[2]} paddingRight={theme.sizes.gutters[2]}>
-                <Tabs isFixed selectedTab={2}>
-                  {[1, 2].map((sem) => (
-                    <Tab key={sem} value={sem}>Semester {sem}</Tab>
-                  ))}
-                </Tabs>
-              </Box>
-              {[1, 2].map(sem => {
-                return (
-                  <TabContent key={sem} value={sem}>
-                    <Semester yr={yr} sem={sem} theme={theme}/>
-                  </TabContent>)
-              })}
-            </TabContext>
-          </Collapsible>
-        </Card>
-      ))}
-    </Container>
+    <FinalContext.provider value={finalData}>
+      <FinalDispatch.provider value={dispatch}>
+        <Container maxWidth={'md'}>
+          <Box marginTop={theme.sizes.gutters[4]}/>
+          {[2021, 2020, 2019, 2018].map((yr, idx) => (
+            <Card key={yr} style={{marginBottom: theme.sizes.gutters[4], paddingLeft: 0, paddingRight: 0}}>
+              <Collapsible collapsed={idx === 0}
+                           isHighlighted={false}
+                           header={
+                             <Box paddingTop={theme.sizes.gutters[2]} paddingBottom={theme.sizes.gutters[2]}>
+                               <Text fSize={'medium'}
+                                     fWeight={'bold'}
+                                     tColor={theme.palette.dark.main}>
+                                 {yr === 2018 ? "Bachelor of Arts [B-ART]"
+                                   : "Bachelor of Social Sciences [BSS]"} - {yr}</Text>
+                             </Box>}
+                           marginBottom={theme.sizes.gutters[4]}>
+                <TabContext>
+                  <Box paddingLeft={theme.sizes.gutters[2]} paddingRight={theme.sizes.gutters[2]}>
+                    <Tabs isFixed selectedTab={2}>
+                      {[1, 2].map((sem) => (
+                        <Tab key={sem} value={sem}>Semester {sem}</Tab>
+                      ))}
+                    </Tabs>
+                  </Box>
+                  {[1, 2].map(sem => {
+                    return (
+                      <TabContent key={sem} value={sem}>
+                        <Semester yr={yr} sem={sem} theme={theme}/>
+                      </TabContent>)
+                  })}
+                </TabContext>
+              </Collapsible>
+            </Card>
+          ))}
+        </Container>
+      </FinalDispatch.provider>
+    </FinalContext.provider>
   );
 };
 
