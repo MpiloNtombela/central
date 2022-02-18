5e2561f133710c610e1c7deb30c66144db4e72c4
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-18 23:55:40 +0200

    Added registered qualifications component

--- src/components/pages/admin/Registration.js
+++ src/components/pages/admin/Registration.js
@@ -1,10 +1,75 @@
+import {useTheme} from "@emotion/react";
 import React from 'react';
+import {useDataContext} from "../../../hooks/context";
+import Table, {TableData, TableHead, TableRow, TBody, THead} from "../../elements/Table";
+import Text from "../../elements/Text";
+import Box from "../../layouts/Box";
+import Container from "../../layouts/Container";
+
+const RegisteredQual = () => {
+  const data = useDataContext()
+  const applications = [...data.applications].filter(app => app.status.toLowerCase() === 'accept offer').sort((a, b) => b.year - a.year)
+
+  return (
+    <Table tableSize={'lg'} striped responsive headColor={'info'}>
+      <THead>
+        <TableRow>
+          <TableHead>Year</TableHead>
+          <TableHead>Qual code</TableHead>
+          <TableHead>Qual name</TableHead>
+          <TableHead>Approved Description</TableHead>
+          <TableHead>College</TableHead>
+          <TableHead>Registration Date</TableHead>
+          <TableHead>Majors</TableHead>
+        </TableRow>
+      </THead>
+      <TBody>
+        {[2021, 2020, ...applications].map((app) => (
+            [2, 1].map(sem => {
+              if (typeof app === 'object') {
+                return (
+                  <TableRow key={sem}>
+                    <TableData>{app.year}:{sem}</TableData>
+                    <TableData>{app.qualification}</TableData>
+                    <TableData>{app.description}</TableData>
+                    <TableData>{app.description}</TableData>
+                    <TableData>{app.college}</TableData>
+                    <TableData>02/02/{app.year}</TableData>
+                    <TableData>First Major, Second Major</TableData>
+                  </TableRow>
+                )
+              } else {
+                return (
+                  <TableRow key={sem}>
+                    <TableData>{app}:{sem}</TableData>
+                    <TableData>{applications[0].qualification}</TableData>
+                    <TableData>{applications[0].description}</TableData>
+                    <TableData>{applications[0].description}</TableData>
+                    <TableData>{applications[0].college}</TableData>
+                    <TableData>02/02/{app}</TableData>
+                    <TableData>First Major, Second Major</TableData>
+                  </TableRow>
+                )
+              }
+            })
+          )
+        )}
+      </TBody>
+    </Table>
+  )
+}
+
 
 const Registration = () => {
-  return (
-    <div>
+  const theme = useTheme()
 
-    </div>
+  return (
+    <Container maxWidth={'lg'}>
+      <Box marginTop={theme.sizes.gutters[4]} marginBottom={theme.sizes.gutters[2]}>
+        <Text fSize={'large'} fWeight={'bold'}>Registered Qualifications</Text>
+      </Box>
+      <RegisteredQual/>
+    </Container>
   );
 };
 
