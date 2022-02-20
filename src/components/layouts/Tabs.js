9b63fe9616e51ccc2bc02f65056dab943a350f67
MpiloNtombela <m.ntombela458@gmail.com>
2022-02-20 23:47:11 +0200

    Added scroll arrows

--- src/components/layouts/Tabs.js
+++ src/components/layouts/Tabs.js
@@ -1,6 +1,7 @@
+import {css} from "@emotion/react";
 import styled from "@emotion/styled";
 import PropTypes from "prop-types";
-import React, {Children, useContext, useLayoutEffect} from 'react';
+import React, {Children, useContext, useLayoutEffect, useRef} from 'react';
 import {useImmerReducer} from "use-immer";
 
 const TabDataContext = React.createContext()
@@ -20,6 +21,47 @@ const tabReducer = (draft, action) => {
       break
   }
 }
+
+const angles = {
+  up: '-135deg',
+  down: '45deg',
+  right: '-45deg',
+  left: '135deg',
+}
+
+const StyledScrollIcon = styled.i`
+  border: solid ${({theme, color}) => color ? color : theme.color.secondary};
+  border-width: 0 5px 5px 0;
+  display: inline-block;
+  padding: 5px;
+  transform: rotate(${({arrowPoint}) => angles[arrowPoint]});
+`
+const StyledScrollArrow = ({theme, indicatorColor}) => css`
+  position: sticky;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${indicatorColor ? indicatorColor : theme.palette.primary.glass};
+  top: 0;
+  bottom: 0;
+  padding: ${theme.sizes.gutters[3]} ${theme.sizes.gutters[2]};
+  list-style: none;
+  border-radius: ${theme.sizes.radius.sm};
+
+  &.hidden {
+    display: none;
+  }
+`
+
+const StyledLeftScrollArrow = styled.ul`
+  ${StyledScrollArrow};
+  left: 0;
+`
+
+const StyledRightScrollArrow = styled.ul`
+  ${StyledScrollArrow};
+  right: 0;
+`
 
 const StyledTab = styled.li`
   padding: ${({theme}) => `${theme.sizes.gutters[3]} ${theme.sizes.gutters[4]}`};
@@ -58,6 +100,7 @@ const StyledTabs = styled.ul`
   scrollbar-width: none;
   box-sizing: border-box;
   box-shadow: ${({elevation}) => elevation ? `0px -5px 20px 0px rgba(0, 0, 0, .${elevation})` : 'none'};
+  position: relative;
 
   &::-webkit-scrollbar {
     display: none;
@@ -94,6 +137,7 @@ const TabContext = ({children}) => {
 export const Tabs = ({children, isFixed, selectedTab, center, style, textColor, indicatorColor, elevation = 0}) => {
   const dispatch = useContext(TabDispatch)
   const childrenTabs = Children.toArray(children).filter(child => child.type === Tab)
+  const tabsEl = useRef(null);
 
   useLayoutEffect(() => {
     if (selectedTab !== false) {
@@ -103,10 +147,34 @@ export const Tabs = ({children, isFixed, selectedTab, center, style, textColor, 
         dispatch({type: CHANGE_TAB, payload: childrenTabs[0]?.props.value})
       }
     }
+    if (tabsEl.current) {
+      tabsEl.current.scrollLeft = 1
+    }
   }, [])
+
+  const handleScroll = (e) => {
+    const tabs = e.currentTarget
+    const sLeft = tabs.querySelector('[data-scroll-left]')
+    const sRight = tabs.querySelector('[data-scroll-right]')
+    const scrollPos = tabs.scrollWidth - tabs.clientWidth
+
+    if (tabs.scrollLeft < 5) {
+      sLeft.classList.add('hidden')
+    } else {
+      sLeft.classList.remove('hidden')
+    }
+    if (tabs.scrollLeft >= (tabs.scrollWidth - tabs.clientWidth)) {
+      sRight.classList.add('hidden')
+    } else {
+      sRight.classList.remove('hidden')
+    }
+  }
   return (
-    <StyledTabs style={style} isFixed={isFixed}
+    <StyledTabs ref={tabsEl} onScroll={handleScroll} data-tabs style={style} isFixed={isFixed}
                 tabCount={childrenTabs?.length} center={center} elevation={elevation}>
+      <StyledLeftScrollArrow color={indicatorColor} data-scroll-left>
+        <StyledScrollIcon color={textColor} arrowPoint='left'/>
+      </StyledLeftScrollArrow>
       {Children.map(children, (child, idx) => {
         if (child.type === Tab) {
           return React.cloneElement(child,
@@ -119,6 +187,9 @@ export const Tabs = ({children, isFixed, selectedTab, center, style, textColor, 
           return child
         }
       })}
+      <StyledRightScrollArrow data-scroll-right>
+        <StyledScrollIcon color={textColor} arrowPoint='right'/>
+      </StyledRightScrollArrow>
     </StyledTabs>
   )
 }
